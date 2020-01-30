eaf7d946f626b97e8d4abb0145eb52a91935012f
Catherine Barnett <ca.barnett06@gmail.com>
2020-01-30 14:53:09 -0500

    adding seeds for schema

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+  
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUE ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUE ('Hot Mess Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUE ('Road House Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUE ('Big Prime Brisket Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUE ('NC BBQ Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUE ('MecktoberFest Burger', true);
